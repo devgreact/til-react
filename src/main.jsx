892535c1c78cef5d0db79da9db79d429727d82ef
devgreact <dev.greact@gmail.com>
2024-12-02 12:10:52 +0900

    JSX 문법

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 
+import Sample5 from "./event-sample/Sample5";
 import "./index.css";
-import EventSample2 from "./event-sample/EventSample2";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <EventSample2 />
+    <Sample5 />
   </>,
 );
