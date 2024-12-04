f5aa60c8e762a1cbbc246cd00910e6c085f4f2d3
devgreact <dev.greact@gmail.com>
2024-12-04 09:59:46 +0900

    fetch 활용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 
-import Sample5 from "./event-sample/Sample5";
 import "./index.css";
+import Member from "./member/Member";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Sample5 />
+    <Member />
   </>,
 );
