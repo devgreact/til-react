f564e336ff06efeb8cf77f1fc22f57314b004d0b
devgreact <dev.greact@gmail.com>
2024-12-05 15:08:21 +0900

    [09-Route 이해]

--- src/apis/config.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const API_URL = "http://localhost:5000/member";
-// axios 인스턴스 생성하기
-export const axiosInstance = axios.create();
---
+++ src/pages/service/Index.jsx
@@ -0,0 +1,5 @@
+function Index() {
+  return <div>/srvice 서비스 소개 페이지</div>;
+}
+
+export default Index;
